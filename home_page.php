6624dfdcc8f127285fdbfa8ef08779928167b8fd
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-02 19:18:08 +0300

    Esimene ül täidetud. Tehtud div lehe keskele, mille tagataustaks on pilt+ valitud teksti kaunistused

---
+++ home_page.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <link type="text/css" rel="stylesheet" href="MMProfileCSS.css"/>
+    <title> Martin's profile page</title>
+</head>
+
+
+<body>
+    <div id="center_txt">Terete</div>
+
+
+</body>
+</html>
+
+
