fda6fddeb0c9213292e0d40bcdab18fd85629e4f
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-03 13:05:20 +0300

    tehtud 3*2 divid, mille sisse saab erinevaid pilte panna. Ka tagatausta värv.

--- home_page.php
+++ home_page.php
@@ -7,8 +7,12 @@
 
 
 <body>
-    <div id="center_txt">Terete</div>
 
+    <div id="outer_div">
+    <div id="first_pic" class="pict">gffgd </div><span id="sec_pic" class="pict">hd</span><span id="third_pic" class="pict">jhdskj</span>
+    <div id="fourth_pic" class="pict">kdk</div><span id="fifth_pic" class="pict">kdhjfkj</span><span id="sixth_pic" class="pict">hdskffhefj</span>
+
+    </div>
 </body>
 </html>
 
