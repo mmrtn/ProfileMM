47931578fbb6410f17f891cdfe7ec424ace2fe00
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-08 11:40:23 +0300

    proovime

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+<!DOCTYPE html>
+<!-- Martini koduleht-->
+<head>
+    <link type="text/css" rel="stylesheet" href="MMProfileCSS.css"/>
+    <title> Martin's profile page</title>
+</head>
+
+
+<body>
+<h1>Martin's <br> favourite <br> things</h1>
+<div id="outer_div">
+    <span id="first_pic" class="pict" ><br>SNOWBOARD</span>
+    <span id="sec_pic" class="pict"><br>CODING</span>
+    <span id="third_pic" class="pict"><br>PHOTOGRAPHY</span>
+    <span id="fourth_pic" class="pict"><br>CAMPING</span>
+    <span id="fifth_pic" class="pict"><br>GOOD FOOD</span>
+    <span id="sixth_pic" class="pict"><br>HOLDEM</span>
+
+</div>
+<table id="table">
+    <thead>
+    <tr>
+        <th colspan="2">Tabel</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td><b>td 1</b></td>
+        <td>See on </td>
+    </tr>
+
+    <tr>
+        <td><b>td 2</b></td>
+        <td>kohustuslik tabel. </td>
+    </tr>
+    <tr>
+        <td><b>td 3</b></td>
+        <td>olema sellel kodulehel.</td>
+    </tr>
+    </tbody>
+</table>
+    <div id="hide">
+        <hr>
+        <h1>Siin</h1>
+
+        <h2>on</h2>
+
+        <h3>erinevad</h3>
+        <h4>käsud,</h4>
+        <h5>mis</h5>
+        <h6>pidid</h6>
+
+        <p>olema</p>
+        <a href="http://www.neti.ee">Üks link ka sekka</a>
+        <br>
+
+        <br>
+        <ol>sellel kodulehel.
+            <li>esimene
+                <ul>
+                    <li>item</li>
+                </ul>
+            </li>
+            <li>teine
+                <ul>
+                    <li>list item</li>
+                </ul>
+            </li>
+        </ol>
+    </div>
+
+
+</body>
+</html>
+
